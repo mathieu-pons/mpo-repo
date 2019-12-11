96fcd8045397d59441bd53b6a224e61b8054bc74
Mathieu Pons <mathieu.pons@gfi.fr>
2019-12-11 09:53:55 +0100

    Add fake service + function clearTodoCompleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//Matérials
+
+//Matérial
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 //Component
 import { AppComponent } from './app.component';
 import { HomeComponent } from './todos/components/home/home.component';
@@ -16,12 +22,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //Store
 import { StoreModule } from '@ngrx/store';
-import { reducer } from './todos/store/reducers';
 import * as fromApp from './store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { DetailTodoComponent } from './todos/components/detail-todo/detail-todo.component';
+
+//Services
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { FakeTodoService } from './todos/services/fake-todo.service';
+import { TodoService } from './todos/services/todo.service';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, ListComponent, AddTodoComponent, DetailTodoComponent],
@@ -34,14 +46,20 @@ import { DetailTodoComponent } from './todos/components/detail-todo/detail-todo.
         MatFormFieldModule,
         MatIconModule,
         FormsModule,
+        HttpClientModule,
+        HttpModule,
+        MatInputModule,
         ReactiveFormsModule,
+        MatTooltipModule,
+        MatButtonModule,
+        HttpClientInMemoryWebApiModule.forRoot(FakeTodoService),
         StoreModule.forRoot(fromApp.reducers, {
             metaReducers: fromApp.metaReducers
         }),
         StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
         !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : []
     ],
-    providers: [],
+    providers: [TodoService],
     bootstrap: [AppComponent],
     entryComponents: [AddTodoComponent]
 })
